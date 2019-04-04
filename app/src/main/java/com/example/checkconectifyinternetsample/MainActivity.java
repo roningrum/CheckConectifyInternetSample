317541fa40a186d9dbb83884b5163fee90ebdf3c
Rizki Oktavia Ningrum <rizkioktavianingrum@gmail.com>
2019-04-04 20:21:20 +0700

    Update snackback notif

--- app/src/main/java/com/example/checkconectifyinternetsample/MainActivity.java
+++ app/src/main/java/com/example/checkconectifyinternetsample/MainActivity.java
@@ -6,21 +6,18 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import static com.example.checkconectifyinternetsample.ConnectivityReceiver.IS_NETWORK_AVAILABLE;
 
 public class MainActivity extends AppCompatActivity{
-    
-    private Button btnCheck;
-    ConnectionDetector connectionDetector;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,11 +33,44 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public void onReceive(Context context, Intent intent) {
                 boolean isNetworkAvailable = intent.getBooleanExtra(IS_NETWORK_AVAILABLE, false);
-                String networkStatus = isNetworkAvailable ? "connected" : "disconnected";
+                String networkStatus;
+                Snackbar snackbar;
+                int color;
 
-                Snackbar.make(findViewById(R.id.activity_main), "Network Status: " + networkStatus, Snackbar.LENGTH_INDEFINITE).show();
+                if (isNetworkAvailable) {
+                    networkStatus = "connected";
+                    color = Color.WHITE;
+                    snackbar = Snackbar.make(findViewById(R.id.activity_main), "Network Status: " + networkStatus, Snackbar.LENGTH_LONG);
+                } else {
+                    networkStatus = "disconnected";
+                    color = Color.WHITE;
+                    snackbar = Snackbar.make(findViewById(R.id.activity_main), "Network Status: " + networkStatus, Snackbar.LENGTH_INDEFINITE)
+                            .setAction("Check Setting", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    startActivity(new Intent(Settings.ACTION_WIRELESS_SETTINGS));
+                                }
+                            }).setActionTextColor(Color.RED);
+                }
+                View sbView = snackbar.getView();
+                TextView textView = sbView.findViewById(android.support.design.R.id.snackbar_text);
+                textView.setTextColor(color);
+                snackbar.show();
+
             }
         }, intentFilter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        checkConnection();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkConnection();
     }
 
 }
